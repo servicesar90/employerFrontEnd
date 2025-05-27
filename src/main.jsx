496f089a5755cd7c22ed371bffd9507c1d1ceff1
servicesar90 <servicesar90@gmail.com>
2025-05-27 15:34:35 +0530

    employer side

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { getDataStore } from './Redux/getDataStore.jsx'
-
+import "@fontsource/poppins"; // Defaults to 400 weight
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
+import "@fontsource/poppins/800.css";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={getDataStore}>
