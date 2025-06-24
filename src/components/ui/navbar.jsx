3dfe9888b39c32e400400d798ec5e3f601424810
servicesar90 <servicesar90@gmail.com>
2025-06-24 16:53:10 +0530

    abcdefg

--- src/components/ui/navbar.jsx
+++ src/components/ui/navbar.jsx
@@ -108,7 +108,7 @@ export default function Navbar() {
                 className="text-gray-700 hover:text-blue-600 transition-colors font-medium cursor-pointer relative group"
                 whileHover={{ y: -2 }}
                 onClick={() => {
-                  navigate(`/${item.pat}`);
+                  navigate(`/${item.path}`);
                 }}
               >
                 {item.label}
